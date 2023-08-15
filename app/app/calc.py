18227bbedbb0cbdfe3677c79320a128647b4fc03
sashoor <sarmadashoor1@gmail.com>
2023-08-14 18:26:51 -0700

    added psycogp2 and docker dependencies to run postgress db

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x,y):
+    """Subtract X and Y an dreturn result."""
+    return y - x
