8d5606c4dfca9ce2083c7c4ded289e2ab74a69ea
sashoor <sarmadashoor1@gmail.com>
2023-08-14 19:06:56 -0700

    adding race condition fix for db start up using core app

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,9 +4,10 @@ Calculator functions
 
 
 def add(x, y):
-    """Add x and y and return result."""
+    """Add x and y and return the result."""
     return x + y
 
-def subtract(x,y):
-    """Subtract X and Y an dreturn result."""
+
+def subtract(x, y):
+    """Subtract x from y and return the result."""
     return y - x
